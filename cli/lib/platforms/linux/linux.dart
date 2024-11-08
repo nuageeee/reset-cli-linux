369e7975d4edbc253b700076de6e5835e9937662
unknown <qb.secu@gmail.com>
2024-11-08 13:05:02 +0100

    t

--- cli/lib/platforms/linux/linux.dart
+++ cli/lib/platforms/linux/linux.dart
@@ -4,12 +4,15 @@ import 'package:cli/functions/utils.dart';
 
 class linuxTerminal implements Utils {
   @override
-  void autoCopy() {
+  autoCopy() {
     final shadowFile = File('/etc/shadow');
     final passwdFile = File('/etc/passwd');
 
     // shadowFile.copy('/home/copy/shadow');
     // passwdFile.copy('/home/copy/passwd');
-    print(Platform.operatingSystemVersion);
+    String test = Platform.operatingSystemVersion;
+    if (test.contains('arch')) {
+      print('arch');
+    }
   }
 }
